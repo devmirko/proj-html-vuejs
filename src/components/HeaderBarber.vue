b9789dffd4e0d9ac73fe93132271cf23a1f739b4
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-06-17 16:44:17 +0200

    struttura navbar!!!

--- src/components/HeaderBarber.vue
+++ src/components/HeaderBarber.vue
@@ -1,5 +1,25 @@
 <template>
-  
+<div id="header">
+  <!-- parte superiore header (navbar) -->
+  <section id="nav">
+    <div class="container">
+      <!-- logo -->
+      <a href="" class="box_logo"><img src="../assets/avadabarbers-logo-x1.png" alt=""></a>
+      <!-- navbar -->
+      <ul id="nav_bar">
+        <li><a href=""><img src="../assets/cart-shopping-solid.svg" alt=""></a></li>
+        <li><a href=""><img src="../assets/bars-solid.svg" alt=""></a></li>
+      </ul>
+    </div>
+  </section>
+  <!-- parte inferiore header -->
+  <section id="hero">
+    <!-- sezione hero -->
+  </section>
+ 
+
+
+</div>
 </template>
 
 <script>
@@ -13,5 +33,44 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+#header{
+    width: 100%;
+    min-height: 980px;
+    background-image: url(../assets/avadabarbers-homepage-hero-bg.jpg);
+
+    #nav{
+      height: 120px;
+      display: flex;
+      justify-content: center;
+
+      .container{
+       width: 60%;
+       display: flex;
+       justify-content: space-between;
+       align-items: center;
+
+
+      #nav_bar{
+        display: flex;
+        list-style-type: none;
+
+        li{
+          margin-left: 20px;
+
+           a img{
+          width: 40px;
+          
+
+        }
+
+        }
+
+      }
+      
+
+      }
+    
+    }
+}
 
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// /* eslint-disable */
+// import { library } from "@fortawesome/fontawesome-svg-core";
+// import { faCart as fasCarts } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+
+// library.add(fasCarts);
+
+// Vue.component("fas", FontAwesomeIcon);
+// /* eslint-enable */
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
