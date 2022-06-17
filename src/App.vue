d130b05fa7078ad68e2970f34dde03b980eb743a
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-06-17 12:31:24 +0200

    inizio progetto!!!

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderBarber/>
+    <!-- parte con il main -->
+    <FooterBarber/>
+   
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderBarber from './components/HeaderBarber.vue'
+import FooterBarber from './components/FooterBarber.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderBarber,
+    FooterBarber
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
